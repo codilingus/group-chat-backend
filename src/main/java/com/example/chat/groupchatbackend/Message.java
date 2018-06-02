69aa1ac4b72ed2b243badcea1c43a5f5b49146a4
Aleksander <aleksanderkotowicz@gmail.com>
2018-06-02 09:38:01 +0200

    message class created

---
+++ src/main/java/com/example/chat/groupchatbackend/Message.java
@@ -0,0 +1,51 @@
+package com.example.chat.groupchatbackend;
+
+import java.time.LocalDateTime;
+
+public class Message {
+
+    private int id;
+    private int senderId;
+    private String text;
+    private LocalDateTime timestamp;
+
+    public Message(int id, int senderId, String text, LocalDateTime timestamp) {
+        this.id = id;
+        this.senderId = senderId;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
