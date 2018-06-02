9b4f0bfe59ef82493a86127d86e06cf3e27d4c6c
Marcin <jmarcin511@gmail.com>
2018-06-02 09:26:25 +0200

    initial-structure

---
+++ group-chat-backend/src/main/java/com/example/chat/groupchatbackend/GroupChatBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.chat.groupchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupChatBackendApplication.class, args);
+	}
+}
