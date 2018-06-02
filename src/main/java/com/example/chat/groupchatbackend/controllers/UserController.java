e19d6d93a7f4d24cb415e8052a02d5cc6835424f
Aleksander <aleksanderkotowicz@gmail.com>
2018-06-02 11:51:50 +0200

    fixed registration endpoint

--- src/main/java/com/example/chat/groupchatbackend/controllers/UserController.java
+++ src/main/java/com/example/chat/groupchatbackend/controllers/UserController.java
@@ -1,15 +1,17 @@
 package com.example.chat.groupchatbackend.controllers;
 
-import com.example.chat.groupchatbackend.Repositories.UserRepository;
+import com.example.chat.groupchatbackend.repositories.UserRepository;
 import com.example.chat.groupchatbackend.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RestController
 public class UserController {
 
     @Autowired
@@ -19,7 +21,9 @@ public class UserController {
     public ResponseEntity register(@RequestBody User user){
         Optional<User> optionalUser = userRepository.findById(user.getId());
         if (optionalUser.isPresent()) {
-            return new ResponseEntity(HttpStatus.MULTI_STATUS);
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("User already exists");
         } else {
             userRepository.save(user);
             return new ResponseEntity(HttpStatus.OK);
