ec256528905844c74f27daa45a05dd4a67f11c6b
Ewelina <evelss005@gmail.com>
2018-06-08 19:59:14 +0200

    delete unnessesery imports

--- src/main/java/com/example/chat/groupchatbackend/controllers/UserController.java
+++ src/main/java/com/example/chat/groupchatbackend/controllers/UserController.java
@@ -17,13 +17,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
